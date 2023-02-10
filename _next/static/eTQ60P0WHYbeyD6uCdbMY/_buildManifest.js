bde8086bad14a884142bf7c65cd473321ae26930
Pham Van Tuan <phamvantuan2304@gmail.com>
2023-02-10 10:16:42 +0700

    update js

--- _next/static/eTQ60P0WHYbeyD6uCdbMY/_buildManifest.js
+++ _next/static/eTQ60P0WHYbeyD6uCdbMY/_buildManifest.js
@@ -1,1 +1,11 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/651-776e749d9bcfa739.js","static/css/539b3fa3dc977d77.css","static/chunks/pages/index-a3aa3229a3d22670.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
+(self.__BUILD_MANIFEST = {
+  __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+  '/': [
+    'static/chunks/651-776e749d9bcfa739.js',
+    'static/css/539b3fa3dc977d77.css',
+    'static/chunks/pages/index-a3aa3229a3d22670.js',
+  ],
+  '/_error': ['static/chunks/pages/_error-a4ba2246ff8fb532.js'],
+  sortedPages: ['/', '/_app', '/_error'],
+}),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
--- static/eTQ60P0WHYbeyD6uCdbMY/_buildManifest.js
+++ static/eTQ60P0WHYbeyD6uCdbMY/_buildManifest.js
@@ -4,6 +4,7 @@
     'static/chunks/651-776e749d9bcfa739.js',
     'static/css/539b3fa3dc977d77.css',
     'static/chunks/pages/index-a3aa3229a3d22670.js',
+    'static/chunks/pages/_app-e80bc048a9519d69.js',
   ],
   '/_error': ['static/chunks/pages/_error-a4ba2246ff8fb532.js'],
   sortedPages: ['/', '/_app', '/_error'],
